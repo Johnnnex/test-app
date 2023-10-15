3204e6ad5541835902332464e80f87508dee5608
Johnnnex <onyeiborjohn05@gmail.com>
2023-10-15 18:31:15 +0100

    login page implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import { DataGet } from "@/context/DataContext";
+import Layout from '@/layout/Layout';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DataGet>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </DataGet>
+  )
 }
