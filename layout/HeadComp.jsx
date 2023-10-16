2570d9d4dfaf930771250d2154447ecbfae69f55
Johnnnex <onyeiborjohn05@gmail.com>
2023-10-16 01:13:06 +0100

    completed table

--- layout/HeadComp.jsx
+++ layout/HeadComp.jsx
@@ -5,18 +5,18 @@ const HeadComp = ({title}) => {
   return (
     <Head>
         <title>{title}</title>
-        <meta name="description" content="This is just to assert that I'm up to the task" />
+        <meta name="description" content="John's version of 'SIGFLOW'" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="sigflow-johnex.vercel.app" />
         <meta name="twitter:title" content="John's Submission . Sigflow" />
-        <meta name="twitter:description" content="This is just to assert that I'm up to the task" />
+        <meta name="twitter:description" content="John's version of 'SIGFLOW'" />
         <meta name="twitter:image" content="https://sigflow-johnex.vercel.app/images/open-graph-img.jpeg" />
         <meta property="og:title" content="John's Submission . Sigflow" />
         <meta property="og:url" content="https://sigflow-johnex.vercel.app" />
         <meta property="og:image" content="https://sigflow-johnex.vercel.app/images/open-graph-img.jpeg" />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content="This is just to assert that I'm up to the task" />
+        <meta property="og:description" content="John's version of 'SIGFLOW'" />
         <meta property="og:site_name" content="John's Submission . Sigflow" />
         <link rel="icon" href="/john.ico" />
     </Head>
